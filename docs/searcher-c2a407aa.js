4a941e8dfd10f07084fd07af8782046ddf1aa567
no_one <thebrokenpipe@proton.me>
2026-03-27 21:41:52 +0000

    edit LIME

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-5bd9d13c.js',
+                path_to_root + 'searchindex-730f0568.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
