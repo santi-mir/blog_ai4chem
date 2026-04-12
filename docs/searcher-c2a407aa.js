b670a1fe7137522ebde597d63aaed63ebe8d7ed5
no_one <thebrokenpipe@proton.me>
2026-04-12 19:05:23 +0100

    add build.

--- docs/searcher-c2a407aa.js
+++ docs/searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-b744f976.js',
+                path_to_root + 'searchindex-b7e5a46f.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
